c28f40f8bf2ec43426fc32c0dd0285357bde1deb
MartkCz <martk494@gmail.com>
2021-10-29 19:34:25 +0200

    Allow type Statement in configuration

--- src/DI/ElasticsearchExtension.php
+++ src/DI/ElasticsearchExtension.php
@@ -5,6 +5,7 @@ namespace Contributte\Elasticsearch\DI;
 use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder;
 use Nette\DI\CompilerExtension;
+use Nette\DI\Definitions\Statement;
 use Nette\Schema\Expect;
 use Nette\Schema\Schema;
 use stdClass;
@@ -20,13 +21,14 @@ class ElasticsearchExtension extends CompilerExtension
 		return Expect::structure([
 			'hosts' => Expect::arrayOf(Expect::anyOf(
 				Expect::string(),
+				Expect::type(Statement::class),
 				Expect::structure([
-					'host' => Expect::string()->required(),
-					'port' => Expect::int(),
-					'scheme' => Expect::string(),
-					'path' => Expect::string(),
-					'user' => Expect::string(),
-					'pass' => Expect::string(),
+					'host' => Expect::anyOf(Expect::string(), Expect::type(Statement::class))->required(),
+					'port' => Expect::anyOf(Expect::int(), Expect::type(Statement::class)),
+					'scheme' => Expect::anyOf(Expect::string(), Expect::type(Statement::class)),
+					'path' => Expect::anyOf(Expect::string(), Expect::type(Statement::class)),
+					'user' => Expect::anyOf(Expect::string(), Expect::type(Statement::class)),
+					'pass' => Expect::anyOf(Expect::string(), Expect::type(Statement::class)),
 				])->castTo('array')
 			))->required()->min(1),
 			'retries' => Expect::int(1),
